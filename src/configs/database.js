f4efd1ceda267996a32b9d2910261f5e9d9dd584
Samuel Fajreldines <samuelfaj@icloud.com>
2023-12-03 00:14:21 -0300

    ✨ feat(database.js): add new database configuration file to manage database connections for different environments using environment variables

---
+++ src/configs/database.js
@@ -0,0 +1,19 @@
+const configuration = {
+	"username": process.env.DB_USER,
+	"password": process.env.DB_PASSWORD,
+	"host": process.env.DB_HOST,
+	"database": process.env.DB_NAME,
+	"dialect": "mysql"
+};
+
+module.exports = {
+	"development": {
+		...configuration,
+	},
+	"test": {
+		...configuration,
+	},
+	"production": {
+		...configuration,
+	}
+};
